5ab1955dde4c4d3dbe6f70dafa2cbc0f979aab8b
OnOlam <salimboyhasanov04@gmail.com>
2026-06-06 10:40:31 +0500

    admin panel

---
+++ Praekt/onolam_backend/create_superuser.py
@@ -0,0 +1,17 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onolam.settings.production')
+django.setup()
+
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@onolam.uz',
+        password='Admin123!@#'
+    )
+    print('Superuser created!')
+else:
+    print('Superuser already exists.')
